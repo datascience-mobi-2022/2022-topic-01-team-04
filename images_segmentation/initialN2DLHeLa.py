220f240fe267ebca4520f8bf76333bf65748a3a6
jadehel <helen.jade@stud.uni-heidelberg.de>
2022-06-06 21:00:01 +0200

    datapackage

---
+++ images_segmentation/initialN2DLHeLa.py
@@ -0,0 +1,21 @@
+gt1 = plt.imread(r'data\Otsu_data\N2DL-HeLa\gt\man_seg13.tif')
+gt2 = plt.imread(r'data\Otsu_data\N2DL-HeLa\gt\man_seg52.tif')
+gt3 = plt.imread(r'data\Otsu_data\N2DL-HeLa\gt\man_seg75.tif')
+gt4 = plt.imread(r'data\Otsu_data\N2DL-HeLa\gt\man_seg79.tif')
+
+
+
+ground_truth_N2DL = [gt1, gt2, gt3, gt4]
+
+
+
+img1 = plt.imread(r'data\Otsu_data\N2DL-HeLa\img\t13.tif')
+img2 = plt.imread(r'data\Otsu_data\N2DL-HeLa\img\t52.tif')
+img3 = plt.imread(r'data\Otsu_data\N2DL-HeLa\img\t75.tif')
+img4 = plt.imread(r'data\Otsu_data\N2DL-HeLa\img\t79.tif')
+
+
+img_N2DL= [img1,img2,img3,img4]
+
+
+
