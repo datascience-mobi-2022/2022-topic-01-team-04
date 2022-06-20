18c5bb60de7556e77f941f2e368552e1632bed07
ieva-so <104202992+ieva-so@users.noreply.github.com>
2022-06-20 11:44:09 +0300

    dsc optimised, commented

--- images_segmentation/dicescore.py
+++ images_segmentation/dicescore.py
@@ -3,42 +3,52 @@ import numpy as np
 import scipy.ndimage
 import matplotlib.pyplot as plt
 
+#create a list for ground truth images and thresholded images (is done manually before running the dice score algorithm, supports iteration trough a whole dataset, here empty lists included just for the package to function)
 ground_truth=[]
 thresholded=[]
-def b_dice(x,gt): # POSITIVE PIXELS = BACKGROUND x=picture number, same number for gt and post tresholding image):
-    gt = ground_truth[x] #gt picture x
-    pt = thresholded[x] #post tresholding picture x
-    tp=0
+
+
+# option 1: positive pixels = background pixels. Used for images with foreground > background
+# define the algorithm, x = image coordinate in the ground truth and thresholded lists
+def b_dice(x): 
+
+    #pick the ground truth and thresholded pictures from the lists
+    gt = ground_truth[x] 
+    pt = thresholded[x] 
+
+    #set new variables for count of different pixel types
+    tp=0 
     tn=0
     fp=0
     fn=0
-    h = gt.shape[0]
-    w = gt.shape[1]
-    for x, y in np.ndindex((h,w)):
+    #iterate over all pixel coordinates (gt and pt have the same dimensions)
+    for x, y in np.ndindex((gt.shape[0],gt.shape[1])):
+
+        #if a pixel belongs to background in ground truth
         if gt[x,y]==0:
+            #if it belongs to background in post-otsu
             if pt[x,y]==0:
+                #add it to the true positive variable
                 tp+=1
+            #count all other pixel kinds
             else:
                 fn+=1
         else:
             if pt[x,y]==0:
                 fp+=1
-            else:
-                tn+=1 #not really needed, but does not influence the runtime
+
+                #else: tn+=1 not included, as tn is not needed in calculations
     b_dsc= 2*tp/(2*tp+fn+fp)
     return b_dsc
-    print(b_dsc)
 
-def f_dice(x): # POSITIVE PIXELS = FOREGROUND
+def f_dice(x): # same thing for positive pixels = foreground pixels
     gt = ground_truth[x] 
     pt = tresholded[x] 
     tp=0
     tn=0
     fp=0
     fn=0
-    h = gt.shape[0]
-    w = gt.shape[1]
-    for x, y in np.ndindex((h,w)):
+    for x, y in np.ndindex((gt.shape[0],gt.shape[1])):
         if gt[x,y]!=0:
             if pt[x,y]!=0:
                 tp+=1
@@ -46,65 +56,17 @@ def f_dice(x): # POSITIVE PIXELS = FOREGROUND
                 fn+=1
         else:
             if pt[x,y]!=0:
-                fp+=1
-            else:
-                tn+=1 
+                fp+=1 
     f_dsc= 2*tp/(2*tp+fn+fp)
     return f_dsc
-    print(f_dsc)
-    
 
 
+def dice(x, method):
+    if method=='background' or method=='b':
+        b_dice(x)
+    if method=='foreground' or method == 'f':
+        f_dice(x)
+    else:
+        print('write <foreground> or <f> for forward dsc and <bacground> or <b> for background dsc')
 
-bice = []
-for i in range(0,18):
-    bice.append(b_dice(i))
-
-print(bice)
-print(mean(bice))
-
-
-
-
-fice = []
-for i in range(0,18):
-    fice.append(f_dice(i))
-
-print(fice)
-print(mean(fice))
-
-
-
-
-def dice(x): #x=picture number, same number for gt and post tresholding image):
-    gt = ground_truth[x] #somehow define that we are taking gt picture x
-    pt = tresholded[x] #somehow define that we are taking post tresholding picture x
-    tp=0
-    tn=0
-    fp=0
-    fn=0
-    for i in shape(gt): # shape(gt) should be equalt to shape(pt)
-        if gt[i]==256:
-            if pt[i]==256:
-                tp+=1
-            else:
-                fn+=1
-        else:
-            if pt[i]==256:
-                fp+=1
-            else:
-                tn+=1 #this part not rly needed tbh
-    dsc= 2*tp/(2*tp+fn+fp)
-    return dsc
-
-
-
-    # we can then make it run trough a whole dataset like
-def dice_avg(y):
-    sum = 0
-    for x in range(0,len(tresholded)+1):
-        dice(x)
-        sum += dsc
-    dsc_avg = sum/len(tresholded)
-    return dsc_avg
 
