a0404a47812b1ad2ff4d89af12d5a3d5f9772908
MaSchneider02 <amelie.maribel.schneider@stud.uni-heidelberg.de>
2022-07-02 14:20:03 +0200

    .

--- images_segmentation/preprocessing.py
+++ images_segmentation/preprocessing.py
@@ -30,3 +30,29 @@ def stretch(x):
     img_min = min(intensities)
     img_stretch = (img-img_min)*(256 / (img_max-img_min))
     return img_stretch
+
+
+
+
+# Preprocessing - Boxplot
+
+def dataset_boxplot(data , title , plot = True):
+    
+    fig_1 = plt.figure(figsize = (14 , 10))
+    ax = fig_1.add_axes([0 , 0 , 1 , 1])
+    ax.set_xticklabels(['No preprocessing' , 'Median filter' , 'Gaussian filter' , 'Histogram \n stretching' , 'Median filter and \n histogram stretching' , 'Histogram stretching and \n median filter' , 'Gaussian filter and \n histogram stretching' , 'Histogram stretching and \n gaussian filter'])
+    plt.title(title , size = 18)
+    plt.ylabel('Preprocessing methods' , size = 14)
+    plt.xlabel('Dice score' , size = 14)
+
+
+    bp = ax.boxplot(data, patch_artist = True , showmeans = True , meanline = True , meanprops = dict(color = "white" , linewidth = 1.5))
+    colors = ['#FF3030', '#FF7F24','#FFB90F', '#BCEE68' , '#00B2EE' , '#BF3EFF' , '#8B4513' , '#808A87']
+   
+    for patch, color in zip(bp['boxes'], colors):
+        patch.set_facecolor(color)
+
+    for median in bp['medians']:
+        median.set(color = 'black' , linewidth = 1)
+    
+    plt.legend([bp["medians"][0]] , ["median"] , loc = 'lower right')
