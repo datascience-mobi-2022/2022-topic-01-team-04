73eb939988482d8ad2a39cd104b2039cc0564200
MaSchneider02 <amelie.maribel.schneider@stud.uni-heidelberg.de>
2022-07-13 12:52:32 +0200

    dti

--- nuclei_segmentation/preprocessing.py
+++ nuclei_segmentation/preprocessing.py
@@ -62,14 +62,14 @@ def stretch(x):
 def dataset_boxplot_global_otsu(data , title , plot = True):
     ymax = max(max(data))
     ymin = min(min(data))
-    if ymin >=0.05:
+    if ymin >= 0.05:
         floor = (math.floor(ymin * 10)) / 10 - 0.05
     else:
-        floor= 0.00
-    if ymax<=0.95:
+        floor = 0.00
+    if ymax <= 0.95:
         ceil = (math.ceil(ymax * 10)) / 10 + 0.05
     else:
-        ceil=1.00
+        ceil = 1.00
     fig_1 = plt.figure(figsize = (14 , 10))
     ax = fig_1.add_axes([0 , 0 , 1 , 1])
     ax.set_xticklabels(['No preprocessing' , 'Median filter' , 'Gaussian filter' , 'Histogram \n stretching' , 'Histogram stretching and \n median filter' , 'Histogram stretching and \n gaussian filter'])
@@ -94,159 +94,3 @@ def dataset_boxplot_global_otsu(data , title , plot = True):
 
 
 
-def dataset_boxplot_two_level_otsu(data , title , yaxis , plot = True):
-    import matplotlib.pyplot as plt
- 
-    fig_1 = plt.figure(figsize = (14 , 10))
-    ax = fig_1.add_axes([0 , 0 , 1 , 1])
-    ax.set_xticklabels(['No preprocessing' , 'Median filter' , 'Gaussian filter' , 'Histogram \n stretching' , 'Histogram stretching and \n median filter' , 'Histogram stretching and \n gaussian filter'])
-    if yaxis == 0:
-        ax.set_ylim([0.2 , 0.95])
-    if yaxis == 1:
-        ax.set_ylim([0.6, 0.95])
-    if data == 2:
-        ax.set_ylim([0.3 , 1])
-
-    plt.title(title , size = 18)
-    plt.ylabel('Preprocessing methods' , size = 14)
-    plt.xlabel('Dice score' , size = 14)
-
-
-    bp = ax.boxplot(data, patch_artist = True , showmeans = True , meanline = True , meanprops = dict(color = "white" , linewidth = 1.5))
-    colors = ['#FF3030', '#FF7F24','#FFB90F', '#BCEE68' , '#00B2EE' , '#BF3EFF']
-   
-    for patch, color in zip(bp['boxes'], colors):
-        patch.set_facecolor(color)
-
-    for median in bp['medians']:
-        median.set(color = 'black' , linewidth = 1)
-    
-    print(bp["means"][0])
-    plt.legend([bp["medians"][0], bp["means"][0]] , ["median", 'mean'], loc = 'lower right' , facecolor = 'gray')
-
-
-
-def dataset_boxplot_two_level_otsu_clip(data , title , yaxis , plot = True):
-    import matplotlib.pyplot as plt
- 
-    fig_1 = plt.figure(figsize = (14 , 10))
-    ax = fig_1.add_axes([0 , 0 , 1 , 1])
-    ax.set_xticklabels(['No preprocessing' , 'Median filter' , 'Gaussian filter' , 'Histogram \n stretching' , 'Histogram stretching and \n median filter' , 'Histogram stretching and \n gaussian filter'])
-    if yaxis == 0:
-        ax.set_ylim([0 , 1])
-    if yaxis == 1:
-        ax.set_ylim([0, 1])
-    if data == 2:
-        ax.set_ylim([0.1 , 0.9])
-
-    plt.title(title , size = 18)
-    plt.ylabel('Preprocessing methods' , size = 14)
-    plt.xlabel('Dice score' , size = 14)
-
-
-    bp = ax.boxplot(data, patch_artist = True , showmeans = True , meanline = True , meanprops = dict(color = "white" , linewidth = 1.5))
-    colors = ['#FF3030', '#FF7F24','#FFB90F', '#BCEE68' , '#00B2EE' , '#BF3EFF']
-   
-    for patch, color in zip(bp['boxes'], colors):
-        patch.set_facecolor(color)
-
-    for median in bp['medians']:
-        median.set(color = 'black' , linewidth = 1)
-    
-    print(bp["means"][0])
-    plt.legend([bp["medians"][0], bp["means"][0]] , ["median", 'mean'], loc = 'lower right' , facecolor = 'gray')
-
-
-def dataset_boxplot_local_otsu(data , title , yaxis , plot = True):
-    import matplotlib.pyplot as plt
- 
-    fig_1 = plt.figure(figsize = (14 , 10))
-    ax = fig_1.add_axes([0 , 0 , 1 , 1])
-    ax.set_xticklabels(['No preprocessing' , 'Median filter' , 'Gaussian filter' , 'Histogram \n stretching' , 'Histogram stretching and \n median filter' , 'Histogram stretching and \n gaussian filter'])
-    if yaxis == 0:
-        ax.set_ylim([0 , 0.85])
-    if yaxis == 1:
-        ax.set_ylim([0.6, 0.85])
-    if data == 2:
-        ax.set_ylim([0 , 1])
-
-    plt.title(title , size = 18)
-    plt.ylabel('Preprocessing methods' , size = 14)
-    plt.xlabel('Dice score' , size = 14)
-
-
-    bp = ax.boxplot(data, patch_artist = True , showmeans = True , meanline = True , meanprops = dict(color = "white" , linewidth = 1.5))
-    colors = ['#FF3030', '#FF7F24','#FFB90F', '#BCEE68' , '#00B2EE' , '#BF3EFF']
-   
-    for patch, color in zip(bp['boxes'], colors):
-        patch.set_facecolor(color)
-
-    for median in bp['medians']:
-        median.set(color = 'black' , linewidth = 1)
-    
-    print(bp["means"][0])
-    plt.legend([bp["medians"][0], bp["means"][0]] , ["median", 'mean'], loc = 'lower right' , facecolor = 'gray')
-
-
-
-def dataset_boxplot_local_two_level_otsu(data , title , yaxis , plot = True):
-    import matplotlib.pyplot as plt
- 
-    fig_1 = plt.figure(figsize = (14 , 10))
-    ax = fig_1.add_axes([0 , 0 , 1 , 1])
-    ax.set_xticklabels(['No preprocessing' , 'Median filter' , 'Gaussian filter' , 'Histogram \n stretching' , 'Histogram stretching and \n median filter' , 'Histogram stretching and \n gaussian filter'])
-    if yaxis == 0:
-        ax.set_ylim([0 , 0.85])
-    if yaxis == 1:
-        ax.set_ylim([0.6, 0.85])
-    if data == 2:
-        ax.set_ylim([0 , 1])
-
-    plt.title(title , size = 18)
-    plt.ylabel('Preprocessing methods' , size = 14)
-    plt.xlabel('Dice score' , size = 14)
-
-
-    bp = ax.boxplot(data, patch_artist = True , showmeans = True , meanline = True , meanprops = dict(color = "white" , linewidth = 1.5))
-    colors = ['#FF3030', '#FF7F24','#FFB90F', '#BCEE68' , '#00B2EE' , '#BF3EFF']
-   
-    for patch, color in zip(bp['boxes'], colors):
-        patch.set_facecolor(color)
-
-    for median in bp['medians']:
-        median.set(color = 'black' , linewidth = 1)
-    
-    print(bp["means"][0])
-    plt.legend([bp["medians"][0], bp["means"][0]] , ["median", 'mean'], loc = 'lower right' , facecolor = 'gray')
-
-
-
-def dataset_boxplot_local_two_level_otsu_clip(data , title , yaxis , plot = True):
-    import matplotlib.pyplot as plt
- 
-    fig_1 = plt.figure(figsize = (14 , 10))
-    ax = fig_1.add_axes([0 , 0 , 1 , 1])
-    ax.set_xticklabels(['No preprocessing' , 'Median filter' , 'Gaussian filter' , 'Histogram \n stretching' , 'Histogram stretching and \n median filter' , 'Histogram stretching and \n gaussian filter'])
-    if yaxis == 0:
-        ax.set_ylim([0 , 0.85])
-    if yaxis == 1:
-        ax.set_ylim([0.6, 0.85])
-    if data == 2:
-        ax.set_ylim([0 , 1])
-
-    plt.title(title , size = 18)
-    plt.ylabel('Preprocessing methods' , size = 14)
-    plt.xlabel('Dice score' , size = 14)
-
-
-    bp = ax.boxplot(data, patch_artist = True , showmeans = True , meanline = True , meanprops = dict(color = "white" , linewidth = 1.5))
-    colors = ['#FF3030', '#FF7F24','#FFB90F', '#BCEE68' , '#00B2EE' , '#BF3EFF']
-   
-    for patch, color in zip(bp['boxes'], colors):
-        patch.set_facecolor(color)
-
-    for median in bp['medians']:
-        median.set(color = 'black' , linewidth = 1)
-    
-    print(bp["means"][0])
-    plt.legend([bp["medians"][0], bp["means"][0]] , ["median", 'mean'], loc = 'lower right' , facecolor = 'gray')
