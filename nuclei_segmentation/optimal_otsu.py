3f97e3f0c08d42febc311c7a8fcf20d80dcc6499
jadehel <helen.jade@stud.uni-heidelberg.de>
2022-07-14 20:51:58 +0200

    optimalotsu

--- nuclei_segmentation/optimal_otsu.py
+++ nuclei_segmentation/optimal_otsu.py
@@ -28,10 +28,10 @@ def dataset_boxplot_optimal_NIH3T3(data , plot = True):
     aa = [0.9080295797878731, 0.9080746699707551, 0.8240269286695534, 0.7523092096313426, 0.7667266042242559, 0.7210834846002242, 0.6796112266384551, 0.7467162829788587, 0.1537407710034592, 0.61035447425189, 0.6454520836896415, 0.676574551172507, 0.5441254230662401, 0.5872934791498239, 0.6104348417019902, 0.6002316933870171, 0.737465149556319, 0.7870066342425532]
     ba = [0.9417263695042412, 0.9439370328532631, 0.5938264317277092, 0.758336987687637, 0.7993590316749856, 0.7272126154647645, 0.6814227277548294, 0.7181630754976537, 0.39260486580391374, 0.6171748562804691, 0.6454109779570866, 0.41456195234474275, 0.49592209998602965, 0.5905319096281916, 0.5107143326635717, 0.6345225131588169, 0.7434112841158013, 0.40626796678720645]
     ca = 
-    data = [ aa, ba, ca, 
+    data = [aa, ba, ca, 
     fig_1 = plt.figure(figsize = (14 , 10))
     ax = fig_1.add_axes([0 , 0 , 1 , 1])
-    ax.set_xticklabels(['global otsu /n histogram stretching median' , 'two level otsu /n no preprocessing', 'two level otsu clip', ' local adaptive thresholding counts', 'local adaptive thresholding average' ])
+    ax.set_xticklabels(['global otsu \n histogram stretching median' , 'two level otsu \n no preprocessing', 'two level otsu clip', ' local adaptive thresholding counts', 'local adaptive thresholding average' ])
     ax.set_ylim([0, 1])
 
     plt.title('optimal Otsu NIH3T3', size = 18)
@@ -63,7 +63,7 @@ def dataset_boxplot_optimal_N2DH_GOWT1(data , plot = True):
 
     fig_1 = plt.figure(figsize = (14 , 10))
     ax = fig_1.add_axes([0 , 0 , 1 , 1])
-    ax.set_xticklabels(['global otsu /n histogram stretching' , 'two level otsu /n median filter', 'two level otsu clip'])
+    ax.set_xticklabels(['global otsu \n histogram stretching' , 'two level otsu \n median filter', 'two level otsu clip'])
     ax.set_ylim([0, 1])
 
     plt.title( 'Optimal Otsu N2DH-GOWT1' , size = 18)
@@ -97,7 +97,7 @@ def dataset_boxplot_optimal_N2DL_HeLa(data, plot= True):
     
     fig_1 = plt.figure(figsize = (14 , 10))
     ax = fig_1.add_axes([0 , 0 , 1 , 1])
-    ax.set_xticklabels(['global otsu /n histogram stretching median' , 'two level otsu /n histogram stretching', 'two level otsu clip', 'local adaptive thresholding average'])
+    ax.set_xticklabels(['global otsu \n histogram stretching median' , 'two level otsu \n histogram stretching', 'two level otsu clip', 'local adaptive thresholding average'])
     ax.set_ylim([0, 1])
 
     plt.title( 'Optimal Otsu N2DL-HeLa' , size = 18)
