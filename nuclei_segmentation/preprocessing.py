cc4336714ad89530028427447837c6dd97449d48
marieindilewitsch <93592623+marieindilewitsch@users.noreply.github.com>
2022-07-15 17:59:19 +0200

    subplot

--- nuclei_segmentation/preprocessing.py
+++ nuclei_segmentation/preprocessing.py
@@ -118,5 +118,18 @@ def dataset_boxplot_otsu(data , title , plot = True):
     #add legend 
     plt.legend([bp["medians"][0], bp["means"][0]] , ["median", 'mean'], loc = 'lower right' , facecolor = 'gray')
 
-
+def subplot(image1, image2, image3, title1, title2, title3, plot=True):
+    fig, ax = plt.subplots(1, 3, figsize=(10,900))
+   
+    ax[0].imshow(image1, 'gray')
+    ax[0].set_title(title1, pad=10, loc="left")
+    ax[1].imshow(image2, 'gray')
+    ax[1].set_title(title2, pad=10, loc="left")
+    ax[2].imshow(image3, 'gray')
+    ax[2].set_title(title3, pad=10, loc="left")
+    ax[0].set_axis_off()
+    ax[1].set_axis_off()
+    ax[2].set_axis_off()
+    plt.tight_layout()
+    fig.show()
 
