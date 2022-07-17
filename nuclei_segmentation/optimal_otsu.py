f8eca502f2e5ffad533a53bcec979814510694d4
jadehel <helen.jade@stud.uni-heidelberg.de>
2022-07-17 16:10:29 +0200

    optimalotsu

--- nuclei_segmentation/optimal_otsu.py
+++ nuclei_segmentation/optimal_otsu.py
@@ -2,22 +2,7 @@
 
 #import packages 
 import matplotlib.pyplot as plt
-import numpy as np
-import scipy.ndimage
-import cv2
-from nuclei_segmentation import otsu as ot
-from nuclei_segmentation import dicescore as dsc
-from nuclei_segmentation import preprocessing as pp
-from nuclei_segmentation import local_thresholding as lt
-from nuclei_segmentation import two_level_otsu as tlo
-import statistics as st
-import os
-import os.path
-import scipy
-
 from matplotlib import pylab, mlab
-
-from IPython.core.pylabtools import figsize, getfigs
 
 
 #optimal otsu boxplot NIH3T3
